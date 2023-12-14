2ddec7703275bc9b401476ae199fe9e1a88968a0
Ishikajain02 <ishijain02@gmail.com>
2023-12-15 00:32:09 +0530

    minor changes

--- frontend/src/components/Emplogs.js
+++ frontend/src/components/Emplogs.js
@@ -6,7 +6,7 @@ return(
       <h1 className=' text-center font-semibold text-lg  p-2'>Employee Activity Logs</h1>
                         <div className=' flex h-22 w-52 border-b-4 border-white p-3 mr-3'>
                             <div>
-                            <img src={emp} className='rounded-full h-16 w-18'></img>
+                            <img src={emp} alt='emp' className='rounded-full h-16 w-18'></img>
                             <h1 >lorem</h1>
                             </div>
                           
@@ -17,7 +17,7 @@ return(
                         </div>
                         <div className=' flex h-22 w-52 border-b-4 border-white p-3 mr-3'>
                             <div>
-                            <img src={emp} className='rounded-full h-16 w-18'></img>
+                            <img src={emp} alt='emp'className='rounded-full h-16 w-18'></img>
                             <h1 >lorem</h1>
                             </div>
                           
@@ -28,7 +28,7 @@ return(
                         </div>
                         <div className=' flex h-22 w-52 border-b-4 border-white p-3 mr-3'>
                             <div>
-                            <img src={emp} className='rounded-full h-16 w-18'></img>
+                            <img src={emp} alt='emp'className='rounded-full h-16 w-18'></img>
                             <h1 >lorem</h1>
                             </div>
                           
@@ -39,7 +39,7 @@ return(
                         </div>
                         <div className=' flex h-22 w-52 border-b-4 border-white p-3 mr-3'>
                             <div>
-                            <img src={emp} className='rounded-full h-16 w-18'></img>
+                            <img src={emp} alt ='emp'className='rounded-full h-16 w-18'></img>
                             <h1 >lorem</h1>
                             </div>
                           
@@ -50,7 +50,7 @@ return(
                         </div>
                         <div className=' flex h-22 w-52 border-b-4 border-white p-3 mr-3'>
                             <div>
-                            <img src={emp} className='rounded-full h-16 w-18'></img>
+                            <img src={emp} alt='emp'className='rounded-full h-16 w-18'></img>
                             <h1 >lorem</h1>
                             </div>
                           
--- frontend/src/components/LoginLeft.js
+++ frontend/src/components/LoginLeft.js
@@ -1,10 +1,12 @@
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
-
+import logo from '../assets/img/logo.jpeg';
 const LoginLeft=()=>{
     return(
         <div className="md:w-1/3  ">
-        <div className=" md:mt-40 mt-10 md:ml-24 md:w-80">
+         <img className="h-28 w-64  mt-24 ml-28" src={logo} alt='logo'></img>
+        <div className=" md:mt-0 md:ml-24 md:w-80">
+           
           <h1 className="font-bold  p-4 m-2 text-4xl">Welcome Back!</h1>
           <h1 className="overflow-hidden break-words p-3 m-2">
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -12,9 +14,7 @@ const LoginLeft=()=>{
             ever since the 1500s</h1>
           
           
-          <button className="bg-gray-300 hover:bg-white rounded m-2  p-2 font-bold  mt-2 md:mt-0  md:ml-12 w-48 ">
-          <FaGoogle className="inline m-2"/>Login with Google</button>
-            <button className="bg-gray-300 hover:bg-white rounded m-2 p-1 font-bold mt-2 md:ml-12  md:mt-0 w-48 h-12"><FaFacebook className="inline m-1"/>Login with Facebook</button>
+         
             </div>
       </div>
     )
